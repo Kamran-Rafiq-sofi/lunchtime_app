6c56b10b6e7ce884376b4d652fa46a40fd6e3195
Kamran-Rafiq-Sofi <kamranrafiq805@gmail.com>
2023-10-18 11:47:17 +0530

    lunchtime app which show what to have according to time

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const date=new Date();
+  const time=date.getHours();
+  if(time<12){
+    return(
+    <h1 style={{color:"black",backgroundColor:"yellow",fontSize:"3em"}}>Breakfast_Time!</h1>
+    )
+  }
+  else if(time >12 && time<18 && time!==15){
+    return(
+    <h1 style={{color:"blue",backgroundColor:"pink",fontSize:"3em"}}>Lunch_Time!</h1>
+    )
+  }
+  else if(time>18){
+    return(
+    <h1 style={{color:"red",backgroundColor:"black",fontSize:"3em"}}>Dinner_Time!</h1>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +34,12 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        
+        {time}
+        <h1>Meal_Time</h1>
+
+
       </header>
     </div>
   );
